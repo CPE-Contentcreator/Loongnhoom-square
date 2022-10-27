32d411aaa1929094bd1884f2c35e0887dba8cbc6
IKNNS <kunanoon1506@gmail.com>
2022-10-28 00:26:40 +0700

    update kwam narak

--- data/menu.js
+++ data/menu.js
@@ -16,6 +16,12 @@
             "nickname": "CHM",
             "description": "The Girls in this Chemistry are cute",
             "price": 40
+        },
+        {
+            "name": "In each year I fell in love for one month",
+            "nickname": "12",
+            "description": "Nong Kon Nun Na Ruk Mak",
+            "price": 40
         }
     ]
 }
